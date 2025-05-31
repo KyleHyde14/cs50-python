2236038cf78450b03bb64057ff3e32cb2384e884
jesushdez <jesushdezjob@gmail.com>
2025-05-31 11:57:57 +0200

    half of week3

--- week3/fuel/fuel.py
+++ week3/fuel/fuel.py
@@ -5,21 +5,22 @@ def main():
         try:
             x, y = fraction.split('/')
             x, y = int(x), int(y)
-
-            percent = round(x/y * 100)
-
             if x > y:
                 fraction = input('Fraction: \n')
-                continue
+            else: 
+                break
 
-            if percent >= 99:
-                return 'F'
-            elif percent <= 1:
-                return 'E'
-            else:
-                return f'{percent}%' 
         except (ValueError, ZeroDivisionError):
             fraction = input('Fraction: \n')
+
+    percent = round(x/y * 100)
+
+    if percent >= 99:
+        return 'F'
+    elif percent <= 1:
+        return 'E'
+    else:
+        return f'{percent}%' 
 
 
 if __name__ == '__main__':
--- week3/taqueria/taqueria.py
+++ week3/taqueria/taqueria.py
@@ -1,1 +1,27 @@
+def main():
+    MENU = {
+        "Baja Taco": 4.25,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
 
+    total = 0
+
+    while True:
+        try:
+            item = input('Item: \n').title()
+        except EOFError:
+            return''
+
+        if item in MENU:
+            total += MENU[item]
+            print(f'Total: ${total:.2f}')
+
+if __name__ == '__main__':
+    print(main())
